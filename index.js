aefea8f446a75724a423380d2176dc658c492d73
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-08-04 23:17:11 +0300

    finished

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.querySelector("main");
+// const secondChild = main.querySelector("li:nth-child(2)");
+// main.removeChild(secondChild);
+main.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+
+newHeader.innerHTML = "YOUR-NAME is the champion";
